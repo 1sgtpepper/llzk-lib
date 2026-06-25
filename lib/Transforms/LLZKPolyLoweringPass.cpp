f807d26c0193089bba0444025796ac911dfc2b79
sgtpepper <cynejarviszarceno@gmail.com>
2026-06-26 03:21:05 +0800

    Reject non-straight-line poly compute bodies (#560)
    * Reject non-straight-line poly compute bodies
    
    Fixes project-llzk/llzk-lib#554.
    
    * Match changelog entry to branch name
    
    * Address straight-line helper review comments
    
    * Format straight-line helper declaration
    
    * Update straight-line unit test helper
    
    ---------
    
    Co-authored-by: Timothy Hoffman <4001421+tim-hoffman@users.noreply.github.com>
    Co-authored-by: Ian Neal <ian@veridise.com>

--- lib/Transforms/LLZKPolyLoweringPass.cpp
+++ lib/Transforms/LLZKPolyLoweringPass.cpp
@@ -487,7 +487,12 @@ class PassImpl : public llzk::impl::PolyLoweringPassBase<PassImpl> {
         return;
       }
 
-      if (failed(checkConstrainBodyIsStraightLine(constrainFunc, "poly lowering"))) {
+      if (failed(checkFuncBodyIsStraightLine(constrainFunc, "poly lowering"))) {
+        signalPassFailure();
+        return;
+      }
+
+      if (failed(checkFuncBodyIsStraightLine(computeFunc, "poly lowering"))) {
         signalPassFailure();
         return;
       }
