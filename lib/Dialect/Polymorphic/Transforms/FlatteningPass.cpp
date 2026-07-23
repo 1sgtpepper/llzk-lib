11fdf510ac93448d5151cbce2e1c45e750b8b37d
1sgtpepper <cynejarviszarceno@gmail.com>
2026-07-23 22:39:56 +0800

    Format specialization encoding changes

--- lib/Dialect/Polymorphic/Transforms/FlatteningPass.cpp
+++ lib/Dialect/Polymorphic/Transforms/FlatteningPass.cpp
@@ -1386,15 +1386,15 @@ public:
 
     SymbolRefAttr originalCalleeAttr = op.getCalleeAttr();
     FailureOr<SymbolRefAttr> newCalleeAttr =
-        layout.remainingNames.empty() ? instantiateFully(
-                                            op, rewriter, symTables, callTgt, parentTemplate,
-                                            parentModule, layout.templateNameWithAttrs,
-                                            paramNameToConcrete
-                                        )
-                                      : instantiatePartially(
-                                            op, rewriter, symTables, callTgt, parentTemplate,
-                                            parentModule, layout, paramNameToConcrete, tracker_
-                                        );
+        layout.remainingNames.empty()
+            ? instantiateFully(
+                  op, rewriter, symTables, callTgt, parentTemplate, parentModule,
+                  layout.templateNameWithAttrs, paramNameToConcrete
+              )
+            : instantiatePartially(
+                  op, rewriter, symTables, callTgt, parentTemplate, parentModule, layout,
+                  paramNameToConcrete, tracker_
+              );
     if (failed(newCalleeAttr)) {
       return failure();
     }
