fae8cde894e8ff379706e7dd9b99d04e7d253b85
foldr <danieldominguez05@gmail.com>
2026-07-28 17:14:03 +0200

    PCL Improvements (#627)
    * PCL Improvements
    
    * Apply PR feedback and update tests
    
    * Add missing ops to translate tool
    
    * Changelog
    
    * Formatting
    
    * Remove empty namespace
    
    * Applied the rest of the feedback
    
    * Formatting
    
    * Add missing include
    
    * Avoid shadowing
    
    * Disable false positive error
    
    * Typo
    
    * Code review feedback
    
    * Add more tests for eq folds and fix patterns
    
    * Formatting
    
    * Add LLZK->PCL e2e tests
    
    * Add missing tests

--- backends/pcl/lib/Dialect/Dialect.cpp
+++ backends/pcl/lib/Dialect/Dialect.cpp
@@ -12,7 +12,11 @@
 #include "pcl/Dialect/IR/Ops.h"
 #include "pcl/Dialect/IR/Types.h"
 
+#include "llzk/Dialect/LLZK/IR/AttributeHelper.h"
+
 #include <mlir/IR/DialectImplementation.h>
+
+#include <algorithm>
 
 // TableGen'd implementation files
 #include "pcl/Dialect/IR/Dialect.cpp.inc"
@@ -42,6 +46,10 @@ void pcl::PCLDialect::initialize() {
 
 using namespace pcl;
 
+//===----------------------------------------------------------------------===//
+// PCLDialect
+//===----------------------------------------------------------------------===//
+
 mlir::LogicalResult
 PCLDialect::verifyOperationAttribute(mlir::Operation *op, mlir::NamedAttribute attr) {
   if (attr.getName() == "pcl.prime") {
@@ -54,10 +62,63 @@ PCLDialect::verifyOperationAttribute(mlir::Operation *op, mlir::NamedAttribute a
       return op->emitError() << "'pcl.prime' may only be on builtin.module";
     }
 
-    const llvm::APInt &v = prime.getValue().getValue();
+    const llvm::APInt &v = prime.getValue();
     if (v.isZero() || v.isNegative()) {
       return op->emitError() << "prime must be positive";
     }
   }
   return mlir::success();
+}
+
+mlir::Operation *PCLDialect::materializeConstant(
+    mlir::OpBuilder &builder, mlir::Attribute value, mlir::Type, mlir::Location loc
+) {
+  return llvm::TypeSwitch<mlir::Attribute, mlir::Operation *>(value)
+      .Case<FeltAttr>([&builder, loc](auto attr) -> mlir::Operation * {
+    return builder.create<ConstOp>(loc, attr);
+  })
+      .Case<BoolAttr>([&builder, loc](auto attr) -> mlir::Operation * {
+    if (attr.getValue()) {
+      return builder.create<TrueOp>(loc);
+    } else {
+      return builder.create<FalseOp>(loc);
+    }
+  }).Default([](auto) {
+    llvm_unreachable("unsupported constant attribute");
+    return nullptr;
+  });
+}
+
+//===----------------------------------------------------------------------===//
+// PrimeAttr
+//===----------------------------------------------------------------------===//
+
+namespace {
+/// Implementation of the reduce operation that assumes the inputs have the same bit width.
+static llvm::APInt reduceImpl(const llvm::APInt &value, const llvm::APInt &prime) {
+  auto reduced = value.srem(prime);
+  if (reduced.isNegative()) {
+    reduced += prime;
+  }
+  return reduced;
+}
+} // namespace
+
+FeltAttr PrimeAttr::reduce(FeltAttr attr) {
+  auto P = getValue();
+  const auto &V = attr.getValue();
+
+  // Fast path for equal widths.
+  if (V.getBitWidth() == P.getBitWidth()) {
+    auto X_p = reduceImpl(V, P);
+    assert(X_p.getBitWidth() == P.getBitWidth());
+    return FeltAttr::get(getContext(), X_p);
+  }
+  auto mw = std::max({P.getBitWidth(), V.getBitWidth()}) + 1;
+  // The incoming value could be negative so we need to sign-extend.
+  auto X_m = reduceImpl(V.sext(mw), P.zext(mw));
+  // Truncate the reduced value to the prime's bit width.
+  auto X_p = X_m.trunc(P.getBitWidth());
+  assert(llvm::APInt::isSameValue(X_m, X_p));
+  return FeltAttr::get(getContext(), X_p);
 }
