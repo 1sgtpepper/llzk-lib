5a013d15de301dce74459de49819843afbdbd925
Timothy Hoffman <4001421+tim-hoffman@users.noreply.github.com>
2026-07-28 12:30:43 -0500

    Update `llzk-pod-to-scalar` to split pods within arrays to multiple arrays (#565)

--- include/llzk/Transforms/LLZKConversionUtils.h
+++ include/llzk/Transforms/LLZKConversionUtils.h
@@ -160,7 +160,8 @@ inline mlir::ArrayAttr replicateFunctionNameAttrsAsNeeded(
 ///
 /// This helper forwards both template parameters and affine-map instantiation operands from the
 /// original call while allowing callers to replace the result types and SSA operands that the new
-/// call should use.
+/// call should use. Discardable attributes from the original call are copied so pass-local metadata
+/// remains attached to rebuilt calls.
 inline function::CallOp createCallPreservingInstantiationOperands(
     mlir::Location loc, mlir::TypeRange newResultTypes, function::CallOp oldCall,
     llvm::ArrayRef<mlir::ValueRange> mapOperands, mlir::ValueRange argOperands,
@@ -171,16 +172,41 @@ inline function::CallOp createCallPreservingInstantiationOperands(
     templateParams.append(templateParamsAttr.begin(), templateParamsAttr.end());
   }
 
+  function::CallOp newCall;
   if (oldCall.getMapOperands().empty()) {
-    return rewriter.create<function::CallOp>(
+    newCall = rewriter.create<function::CallOp>(
         loc, newResultTypes, oldCall.getCalleeAttr(), argOperands, templateParams
+    );
+  } else {
+    newCall = rewriter.create<function::CallOp>(
+        loc, newResultTypes, oldCall.getCalleeAttr(), mapOperands, oldCall.getNumDimsPerMapAttr(),
+        argOperands, templateParams
     );
   }
 
-  return rewriter.create<function::CallOp>(
-      loc, newResultTypes, oldCall.getCalleeAttr(), mapOperands, oldCall.getNumDimsPerMapAttr(),
-      argOperands, templateParams
-  );
+  newCall->setDiscardableAttrs(oldCall->getDiscardableAttrDictionary());
+  return newCall;
+}
+
+/// Replace any AffineMap-backed array dimensions within `type` with wildcard `?` dims.
+inline static mlir::Type replaceAffineMapArrayDimsWithWildcards(mlir::Type type) {
+  auto arrTy = llvm::dyn_cast<array::ArrayType>(type);
+  if (!arrTy) {
+    return type;
+  }
+
+  mlir::Builder builder(arrTy.getContext());
+  llvm::SmallVector<mlir::Attribute> dims;
+  dims.reserve(arrTy.getDimensionSizes().size());
+  for (mlir::Attribute dimSize : arrTy.getDimensionSizes()) {
+    if (llvm::isa<mlir::AffineMapAttr>(dimSize)) {
+      dims.push_back(builder.getIndexAttr(mlir::ShapedType::kDynamic));
+    } else {
+      dims.push_back(dimSize);
+    }
+  }
+
+  return arrTy.cloneWith(replaceAffineMapArrayDimsWithWildcards(arrTy.getElementType()), dims);
 }
 
 /// General helper for converting a `FuncDefOp` by changing its input and/or result types and the
--- lib/Dialect/Polymorphic/Transforms/FlatteningPass.cpp
+++ lib/Dialect/Polymorphic/Transforms/FlatteningPass.cpp
@@ -1021,7 +1021,7 @@ public:
       if (!changed && newElemTy == inputTy.getElementType()) {
         return inputTy;
       }
-      return flattenInstantiatedArrayType(
+      return flattenArrayElementType(
           inputTy.cloneWith(inputTy.getElementType(), updated), newElemTy
       );
     });
